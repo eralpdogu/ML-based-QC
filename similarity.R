e2328bcf405013ba12f6d579fd930ac15a818141
Shantam <mailshantam@gmail.com>
2018-05-09 23:31:33 -0400

    starting with similarity matrix

---
+++ similarity.R
@@ -0,0 +1,28 @@
+# Similarity matrix in r
+
+#train data 
+#S0 
+#N0
+
+#test data
+#Data
+#NW
+
+#makes sure the data is numeric
+sw <- sapply(Data[,c(-1,-50)],as.numeric)
+s0 <- sapply(S0[,c(-1,-50)],as.numeric)
+
+#install.packages("resemble")
+library(resemble) # cosine
+?fDiss
+ed <- fDiss(s0,t(as.data.frame(sw[1,])), method = "cosine",center = F, scaled = F)
+
+
+
+
+
+#takes a sample of test data on find simlarity on real time basis
+for(i in 1:length(Data)){
+  mycosine(Data[i,],S0)
+  
+}
