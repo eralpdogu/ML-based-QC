e6ce5a9accb51363336ff1d340c1fcddc89600af
Eralp DOGU <eralp.dogu@gmail.com>
2020-03-06 15:38:09 +0300

    fixed the redundant column

--- Factorial Runs v2/4paper.R
+++ Factorial Runs v2/4paper.R
@@ -20,14 +20,7 @@ Test.set<-test.set.SRM
 
 MSstatsQC.ML.trainR(guide.set, sim.size=2000)
 
-RESPONSE<-NA
 Test.set<-rbind(guide.set[1:500,],test.set.SRM[1:200,])
-test.set<-cbind(Test.set, RESPONSE)
-MSstatsQC.ML.testR(Test.set, guide.set)
-
-RESPONSE<-NA
-Test.set<-rbind(test.set.SRM[1:500,])
-test.set<-cbind(Test.set, RESPONSE)
 MSstatsQC.ML.testR(Test.set, guide.set)
 
 guide.set %>%
@@ -58,10 +51,6 @@ ggplot(test.set.SRM[1:200,], aes(TotalArea))+
 guide.set<-guide.set.DDA[,]
 test.set<-test.set.DDA2[,]
 
-guide.set<-test.set.DDA[,]
-RESPONSE<-NA
-guide.set<-cbind(guide.set, RESPONSE)
-
 MSstatsQC.ML.trainR(guide.set[,], sim.size=1000, guide.set.annotations = NULL)
 MSstatsQC.ML.trainR(guide.set, sim.size=1000, guide.set.annotations = guide.set.DDA.anno)
 MSstatsQC.ML.trainR(guide.set, sim.size=100)
@@ -69,9 +58,6 @@ MSstatsQC.ML.trainR(guide.set, sim.size=100)
 new.test<-rbind(guide.set[,], test.set[test.set$idfile>41234&test.set$idfile<41505 ,])
 new.test<-rbind(guide.set[201:838,], test.set[1:200,])
 MSstatsQC.ML.testR(new.test, guide.set)
-
-new.test<-rbind(guide.set, test.set)
-MSstatsQC.ML.testR(new.test, test.set)
 
 guide.set %>%
   group_by(peptide) %>%
